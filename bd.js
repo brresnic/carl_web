8a0d006387a3903dfbab53fe3b3cd4c851a1452b
brresnic <benjamin_resnick@brown.edu>
2014-10-06 22:29:54 -0400

    everything

---
+++ bd.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('#title').css('opacity',.85);
+	$('#solar-system').fadeIn(750, function(){
+		$('#title').fadeIn(800,function(){
+		});
+		$('#about').fadeIn(2600);
+		$('#red-planet').fadeIn(1900);
+		$('#mondo').fadeIn(1550);
+		$('#music').fadeIn(900);
+		$('#art').fadeIn(1300);
+	});
+});
